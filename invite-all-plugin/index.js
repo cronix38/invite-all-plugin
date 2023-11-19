b8f4f224a205506b367fd04b74f6ea081ab3f920
cronix38 <cronix38@hotmail.com>
2023-11-19 18:22:17 +1100

    v1.01
    Comments, select button div from lobby div instead of querying whole document

--- invite-all-plugin/index.js
+++ invite-all-plugin/index.js
@@ -1,10 +1,19 @@
-const inviteFriends = async () => {
-  const response = await fetch("/lol-chat/v1/friends");
-  const data = await response.json();
-  const onlineFriends = data.filter(
-    (friend) => friend.availability === "chat" || friend.availability === "away"
-  );
+// the name of our new button's div
+const inviteDivId = "inviteAllDiv";
 
+// find our online friends
+const fetchOnlineFriends = async () => {
+  const response = await fetch("/lol-chat/v1/friends");
+  if (!response.ok) {
+    throw new Error(`HTTP error when fetching friends. ${response.status}`);
+  }
+  const data = await response.json();
+  return data.filter(({availability}) => availability === "chat" || availability === "away");
+}
+
+// invite our online friends
+const inviteFriends = async () => {
+  const onlineFriends = await fetchOnlineFriends();
   await fetch("/lol-lobby/v2/lobby/invitations", {
     method: "POST",
     headers: {
@@ -17,35 +26,42 @@ const inviteFriends = async () => {
   });
 };
 
+// try to invite friends and display a toast with the result
+const handleButtonClick = async () => {
+  try {
+    await inviteFriends();
+    Toast.success("Friends invited");
+  } catch (error) {
+    Toast.error("Failed to invite friends", error);
+  }
+};
+
+// create our button
 const createButton = () => {
   const button = document.createElement("lol-uikit-flat-button");
   button.textContent = "Invite All";
-  button.onclick = async () => {
-    try {
-      await inviteFriends();
-      Toast.success("Friends invited");
-    } catch (error) {
-      Toast.error("Failed to invite friends", error);
-    }
-  };
+  button.onclick = handleButtonClick;
   return button;
 };
 
-const createMainDiv = () => {
-  const mainDiv = document.createElement("div");
-  mainDiv.id = "inviteAllDiv";
-  mainDiv.appendChild(createButton());
-  return mainDiv;
+// create a div to hold our button
+const createButtonsDiv = () => {
+  const buttonsDiv = document.createElement("div");
+  buttonsDiv.id = inviteDivId;
+  buttonsDiv.appendChild(createButton());
+  return buttonsDiv;
 };
 
 export function load() {
+  // create an observer that will run when the dom is updated
   new MutationObserver(() => {
+    // select the invisible bar that appears at the top of party creation screen
     const lobbyHeaderDiv = document.querySelector(
       ".lobby-header-buttons-container"
     );
-    const inviteAllDiv = document.querySelector("#inviteAllDiv");
-    if (lobbyHeaderDiv && !inviteAllDiv) {
-      lobbyHeaderDiv.insertBefore(createMainDiv(), lobbyHeaderDiv.children[1]);
+    // if the lobby bar exists and our button does not exist on it, add our button
+    if (lobbyHeaderDiv && !lobbyHeaderDiv.querySelector(`#${inviteDivId}`)) {
+      lobbyHeaderDiv.insertBefore(createButtonsDiv(), lobbyHeaderDiv.children[1]);
     }
   }).observe(document.body, { childList: true, subtree: true });
 }
